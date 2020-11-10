86cc3129ae8bf1fd1e1ca0efc4078a5dc993ba69
PegJac <peggy.jacobson@outlook.com>
2020-11-10 16:01:52 +0100

    todo list

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+class toDo {
+    constructor (toDoItem){
+        this.toDoItem = toDoItem;
+    }
+}
+
 window.onload = function () {
-    alert("hej");
+
+    let todo1 = new toDo('to do 1');
+    let todo2 = new toDo('to do 2');
+    let todo3 = new toDo('to do 3');
+    let todos = [todo1, todo2, todo3];
+
+for (let i = 0; i < todos.length; i++) {
+    let pTag = document.createElement("p");
+    pTag.innerHTML = todos[i].toDoItem;
+
+    document.body.appendChild(pTag);
+}
 }
